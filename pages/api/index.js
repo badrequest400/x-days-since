1539b5cb8cc81ac63eaa90e6b5c2136aef698851
Balazs Cseh <balazs.cseh89@gmail.com>
2020-12-17 18:15:41 +0000

    async

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,7 +4,7 @@ import { promisify } from 'util';
 const client = redis.createClient(process.env.REDIS_URL)
 const getAsync = promisify(client.get).bind(client);
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const tech = req.query.tech;
   const result = await getAsync(tech);
 
