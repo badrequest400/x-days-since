c7570b5a0235bde374b283c740db0b6a1901f444
Balazs Cseh <balazs.cseh89@gmail.com>
2021-02-20 23:29:43 +0000

    CORS

--- pages/api/tech/[tech].js
+++ pages/api/tech/[tech].js
@@ -1,14 +1,33 @@
 import redis from 'redis';
 import { promisify } from 'util';
+import  Cors from 'cors';
 
 const client = redis.createClient(process.env.REDIS_URL)
 const getAsync = promisify(client.get).bind(client);
+
+const runMiddleware = mw => (req, res) =>
+  new Promise((resolve, reject) => {
+    mw(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+
+const cors = Cors({
+  methods: [ 'HEAD', 'GET', 'PUT' ],
+});
+
+const runCors = runMiddleware(cors);
 
 export default async function handler(req, res) {
   const {
     query: { tech },
     method,
   } = req;
+
+  await runCors();
 
   switch (method) {
     case 'GET': {
